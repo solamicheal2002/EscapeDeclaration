b2a17b4af12e4d636edb7a6022ec8ce9436bd67a
Sola Micheal <solamicheal2002@gmail.com>
2017-03-22 09:27:20 +0100

    training

---
+++ EscapeCharacter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World\\");
+
+            Console.WriteLine("Jesus is coming soon\n\a");
+
+            Console.ReadLine();
+        }
+    }
+}
